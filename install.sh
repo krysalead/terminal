7841da30175a7dd19537fa0322dd19d00a22fd00
Krys Alead <krysalead@gmail.com>
2023-05-12 10:05:22 +0000

    extra " removed

--- install.sh
+++ install.sh
@@ -2,5 +2,5 @@
 sudo apt install zsh
 chsh -s $(which zsh)
 git clone https://github.com/lukechilds/zsh-nvm ~/.oh-my-zsh/custom/plugins/zsh-nvm
-git clone "https://github.com/MichaelAquilina/zsh-autoswitch-virtualenv ~/.oh-my-zsh/plugins/autoswitch_virtualenv
+git clone https://github.com/MichaelAquilina/zsh-autoswitch-virtualenv ~/.oh-my-zsh/plugins/autoswitch_virtualenv
 echo ". ~/terminal/.zshrc" > .zshrc
