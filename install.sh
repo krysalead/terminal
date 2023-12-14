97fa53d2b99c6a742894ffd723f4bf0bfeb0b0b2
Krys Alead <krysalead@gmail.com>
2023-12-14 12:46:56 +0000

    Fix the install script

--- install.sh
+++ install.sh
@@ -1,15 +1,17 @@
-!/bin/bash
+#!/usr/bin/env bash
 if [ -z "$1" ]; then
   sudo apt install zsh
   sudo chsh -s $(which zsh)
   sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" -s --unattended
   echo "Cloning plugins code"
   git clone https://github.com/lukechilds/zsh-nvm ~/.oh-my-zsh/custom/plugins/zsh-nvm
-  git clone "https://github.com/MichaelAquilina/zsh-autoswitch-virtualenv.git" "~/.oh-my-zsh/custom/plugins/autoswitch_virtualenv"
+  git clone "https://github.com/MichaelAquilina/zsh-autoswitch-virtualenv.git" ~/.oh-my-zsh/custom/plugins/autoswitch_virtualenv
+  sudo touch $ZSH_CACHE_DIR/dotenv-allowed.list
+  sudo touch $ZSH_CACHE_DIR/dotenv-disallowed.list
   echo "Replace the zsh rc file"
-  echo ". ~/terminal/.zshrc" > .zshrc
+  echo ". ~/terminal/.zshrc" > ~/.zshrc
 fi
-if [ "$1" eq "kube" ]; then
+if [ "$1" == "kube" ]; then
   echo "Installing kubectl"
   curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
   sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
@@ -32,7 +34,7 @@ if [ "$1" eq "kube" ]; then
   echo "alias k='kubectl '" >> ~/terminal/.profile
   echo "alias ingresslogs='kubectl logs -n ingress-nginx -l app=ingress-nginx'" >> ~/terminal/.profile
 fi
-if [ "$1" eq "python" ]; then
+if [ "$1" == "python" ]; then
   sudo add-apt-repository ppa:deadsnakes/ppa
   sudo apt-get update -y
   sudo apt-get install -y python3.10
@@ -41,7 +43,7 @@ if [ "$1" eq "python" ]; then
   python -m pip install --user virtualenv
   rm get-pip.py
 fi
-if [ "$1" eq "node" ]; then
+if [ "$1" == "node" ]; then
   echo "installing nodejs"
   sudo apt update
   sudo apt install -y nodejs
@@ -49,7 +51,7 @@ if [ "$1" eq "node" ]; then
   curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash 
   git clone https://github.com/MichaelAquilina/zsh-autoswitch-virtualenv ~/.oh-my-zsh/plugins/autoswitch_virtualenv
 fi
-if [ "$1" eq "acri" ]; then
+if [ "$1" == "acri" ]; then
   git config --global user.name Olivier DAUMAS
   git config --global user.email olivier.daumas@acri-st.fr
   echo "Installing Minio"
